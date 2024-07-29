c985dbb727746e1ff996395fa85993b0768c406b
Usuario <Usuario@CPD03-M20>
2024-07-29 15:55:01 -0300

    Adicionar arquivos de projeto.

---
+++ MauiShowDoMilhao/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MauiShowDoMilhao.Models;
+
+namespace MauiShowDoMilhao
+{
+    public partial class App : Application
+    {
+        List<Pergunta> perguntas_faceis = new()
+        {
+            new Pergunta
+            {
+                Id = 1,
+                Enunciado = "Em que estado brasileiro nasceu a apresentadora Xuxa?",
+                Alternativas = new()
+                {
+                    new Alternativa { Correta = false, Descricao = "RIO DE JANEIRO" },
+                    new Alternativa { Correta = true, Descricao = "RIO GRANDE DO SUL" },
+                    new Alternativa { Correta = false, Descricao = "SANTA CATARINA" },
+                    new Alternativa { Correta = false, Descricao = "GOIÁS" },
+                }
+            },
+        };
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiShowDoMilhao/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiShowDoMilhao
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
